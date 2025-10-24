1711e2bb0d847380572aa4f28f0bbc883ffa2b43
Shubpawar <46081488+Shubpawar@users.noreply.github.com>
2025-10-24 09:06:50 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,18 @@
+IF EXISTS (SELECT 1 FROM sys.databases where name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
 
+
+
+/* the master database is a system database in Sql Server where you can create other databases*/
+
+CREATE DATABASE DataWarehouse;
+
+CREATE SCHEMA bronze;
+go  --
+CREATE SCHEMA silver;
+go --separate batches when working with multiple SQL statements 
+CREATE SCHEMA gold;
